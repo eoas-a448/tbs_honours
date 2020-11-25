c8e2f8df380aa829ce7a338f0d4649e48b9af958
tgschmidt05 <tgschmidt@shaw.ca>
2020-11-25 11:34:14 -0700

    Made extra script for plotting basic BTD data

--- get_goes.py
+++ get_goes.py
@@ -1,16 +1,16 @@
 import GOES
 
-DATA_DIR = "/Users/tschmidt/repos/tgs_honours/good_data/"
-# DATA_DIR1 = "/Users/tschmidt/repos/tgs_honours/good_data/16-ch7-apr24/" # This one needs trailing / for goes lib
-# DATA_DIR2 = "/Users/tschmidt/repos/tgs_honours/good_data/16-ch14-apr24/" # This one needs trailing / for goes lib
+# DATA_DIR = "/Users/tschmidt/repos/tgs_honours/good_data/"
+DATA_DIR1 = "/Users/tschmidt/repos/tgs_honours/good_data/16-ch7-may13/" # This one needs trailing / for goes lib
+DATA_DIR2 = "/Users/tschmidt/repos/tgs_honours/good_data/16-ch14-may13/" # This one needs trailing / for goes lib
 
-# GOES.download('goes16', 'ABI-L1b-RadF', Channel = ['07'],
-#               DateTimeIni = '20190424-140000', DateTimeFin = '20190425-010000',
-#               Rename_fmt = '%Y%m%d%H%M', PathOut = DATA_DIR1)
-# GOES.download('goes16', 'ABI-L1b-RadF', Channel = ['14'],
-#               DateTimeIni = '20190424-140000', DateTimeFin = '20190425-010000',
-#               Rename_fmt = '%Y%m%d%H%M', PathOut = DATA_DIR2)
+GOES.download('goes16', 'ABI-L1b-RadF', Channel = ['07'],
+              DateTimeIni = '20190513-000000', DateTimeFin = '20190514-000000',
+              Rename_fmt = '%Y%m%d%H%M', PathOut = DATA_DIR1)
+GOES.download('goes16', 'ABI-L1b-RadF', Channel = ['14'],
+              DateTimeIni = '20190513-000000', DateTimeFin = '20190514-000000',
+              Rename_fmt = '%Y%m%d%H%M', PathOut = DATA_DIR2)
 
-GOES.download('goes16', 'ABI-L2-MCMIPF', Channel = ['01'],
-              DateTimeIni = '20190424-140000', DateTimeFin = '20190424-180000',
-              Rename_fmt = '%Y%m%d%H%M', PathOut = DATA_DIR)
+# GOES.download('goes16', 'ABI-L2-MCMIPF', Channel = ['01'],
+#               DateTimeIni = '20190424-140000', DateTimeFin = '20190424-180000',
+#               Rename_fmt = '%Y%m%d%H%M', PathOut = DATA_DIR)
--- make_pngs_BTD.py
+++ make_pngs_BTD.py
@@ -13,28 +13,29 @@ import matplotlib.ticker as mticker
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from cartopy.mpl.gridliner import LONGITUDE_FORMATTER, LATITUDE_FORMATTER
 import os
+import scipy
+import rasterio
+from skimage import feature
+from skimage import filters
+from skimage import morphology
+from skimage import transform
+from skimage.segmentation import active_contour
+from skimage.restoration import denoise_nl_means, estimate_sigma
+from cv2 import cv2
+from PIL import Image
 
 # DATA_DIR_7 = "/Users/tschmidt/repos/tgs_honours/good_data/16-ch7-apr24/"
 # DATA_DIR_14 = "/Users/tschmidt/repos/tgs_honours/good_data/16-ch14-apr24/"
 # DATA_DIR_7 = "/Users/tschmidt/repos/tgs_honours/good_data/16-ch7-sep12/"
 # DATA_DIR_14 = "/Users/tschmidt/repos/tgs_honours/good_data/16-ch14-sep12/"
-DATA_DIR_7 = "/Users/tschmidt/repos/tgs_honours/good_data/16-ch7-aug08/"
-DATA_DIR_14 = "/Users/tschmidt/repos/tgs_honours/good_data/16-ch14-aug08/"
+# DATA_DIR_7 = "/Users/tschmidt/repos/tgs_honours/good_data/16-ch7-aug08/"
+# DATA_DIR_14 = "/Users/tschmidt/repos/tgs_honours/good_data/16-ch14-aug08/"
+DATA_DIR_7 = "/Users/tschmidt/repos/tgs_honours/good_data/16-ch7-may13/"
+DATA_DIR_14 = "/Users/tschmidt/repos/tgs_honours/good_data/16-ch14-may13/"
 OUT_DIR = "/Users/tschmidt/repos/tgs_honours/output/"
 # Defines the plot area
 LLLon, URLon = -135, -116.5
 LLLat, URLat = 28, 38.5
-
-############DATA###############
-
-# GOES.download('goes16', 'ABI-L1b-RadF', Channel = ['07'],
-#               DateTimeIni = '20190807-220000', DateTimeFin = '20190809-020000',
-#               Rename_fmt = '%Y%m%d%H%M', PathOut = DATA_DIR_7)
-# GOES.download('goes16', 'ABI-L1b-RadF', Channel = ['14'],
-#               DateTimeIni = '20190807-220000', DateTimeFin = '20190809-020000',
-#               Rename_fmt = '%Y%m%d%H%M', PathOut = DATA_DIR_14)
-
-###########PROGRAM################
 
 # Get projection info along with axis and fig objects for matplotlib
 fig, ax, MapProj, FieldProj = matplot_consts.main_func()
@@ -81,13 +82,6 @@ loncor, latcor = GOES.get_lonlat_corners(lons, lats)
 # Make BTD
 var = calc_BTD.main_func(var_ch14, var_ch07, 14, 7)
 
-# Plot colorbar
-# cbar = fig.colorbar(img, extend='neither', spacing='proportional',
-#                     orientation = 'horizontal')
-# cbar.ax.tick_params(labelsize=6, labelcolor='black', width=0.5, direction='out', pad=1.0)
-# cbar.set_label(label='Brightness Temperature Difference (BTD)', size=6, color='black', weight='normal')
-# cbar.outline.set_linewidth(0.5)
-
 plot.main_func(var, loncor, latcor, fig, ax, MapProj, FieldProj, os.path.join(OUT_DIR, '0.png'))
 
 i = 1
@@ -116,6 +110,42 @@ for ds_name_7 in data_list_7:
 
     # Make BTD
     var = calc_BTD.main_func(var_ch14, var_ch07, 14, 7)
+
+    # # Filter out the land
+    # # (This is temporarilly hardcoded. Wil use shapefile later with rasterio)
+    # lons = np.where(lons==-999.99, np.nan, lons)
+    # lats = np.where(lats==-999.99, np.nan, lats)
+    # var = np.where(lons > -118.5, np.nan, var)
+    # var = np.where(np.logical_and(lons > -124.5, lats > 33.75), np.nan, var)
+
+    # # Create mask array for the highest clouds
+    # high_cloud_mask = calc_BTD.bt_ch14_temp_conv(var_ch14) < 5 # TODO: Make this more robust
+
+    # #####TESTING######
+    # # Use "golden arches" to filter out open ocean data
+    # kernel_size = (3,3) # 2 or 3 seems optimal
+    # BT = np.where(high_cloud_mask, np.nan, calc_BTD.bt_ch14_temp_conv(var_ch14)) # Remove highest clouds since they are the most irregular
+    # BT_local_mean = scipy.ndimage.filters.generic_filter(BT, np.mean, kernel_size)
+    # BT_local_SD = scipy.ndimage.filters.generic_filter(BT, np.std, kernel_size)
+
+    # # plot.scatter_plt(BT_local_mean, BT_local_SD, "/Users/tschmidt/repos/tgs_honours/output/derp.png")
+
+    # mean_cutoff = np.nanpercentile(BT_local_mean, 50)
+    # SD_cutoff = np.nanpercentile(BT_local_SD, 95)
+
+    # # mean_cutoff = (np.nanmax(BT_local_mean)-np.nanmin(BT_local_mean))*0.70 # Was 0.90
+    # # SD_cutoff = (np.nanmax(BT_local_SD)-np.nanmin(BT_local_SD))*0.33 # Was 0.15
+    # golden_arch_mask = np.logical_and(BT_local_mean > mean_cutoff, BT_local_SD < SD_cutoff)
+
+    # # mean_cutoff = (np.nanmax(BT_local_mean)-np.nanmin(BT_local_mean))*0.65
+    # # SD_cutoff = (np.nanmax(BT_local_SD)-np.nanmin(BT_local_SD))*0.15
+    # # golden_arch_mask = np.logical_or(BT_local_mean > mean_cutoff, BT_local_SD > SD_cutoff)
+
+    # var = np.where(golden_arch_mask, np.nan, var)
+    # #################
+
+    # #Filter out the cold high altitude clouds
+    # var = np.where(high_cloud_mask, np.nan, var)
 
     plot.main_func(var, loncor, latcor, fig, ax, MapProj, FieldProj, file_path)
 
